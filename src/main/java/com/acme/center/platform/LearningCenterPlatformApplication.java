2c3f43cd15c0d7a1dfd80155790e7c61f3717066
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-21 07:22:47 -0500

    chore: initial commit.

---
+++ src/main/java/com/acme/center/platform/LearningCenterPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.center.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCenterPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningCenterPlatformApplication.class, args);
+    }
+
+}
