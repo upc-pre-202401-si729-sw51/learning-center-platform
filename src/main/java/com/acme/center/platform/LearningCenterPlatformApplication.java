42def337263845577c9fa6b65ae821b872ce2498
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-23 07:59:48 -0500

    feat(audit): added application configuration to enable record audit.

--- src/main/java/com/acme/center/platform/LearningCenterPlatformApplication.java
+++ src/main/java/com/acme/center/platform/LearningCenterPlatformApplication.java
@@ -2,8 +2,10 @@ package com.acme.center.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LearningCenterPlatformApplication {
 
     public static void main(String[] args) {
