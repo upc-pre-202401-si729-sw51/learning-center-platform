437531d6e848631c6cfeb0815e05a1b6ff680941
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-04 07:44:53 -0500

    feat(iam): added user aggregate.

---
+++ src/main/java/com/acme/center/platform/iam/domain/model/aggregates/User.java
@@ -0,0 +1,56 @@
+package com.acme.center.platform.iam.domain.model.aggregates;
+
+import com.acme.center.platform.iam.domain.model.entities.Role;
+import com.acme.center.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class User extends AuditableAbstractAggregateRoot<User> {
+
+    @Getter
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true)
+    private String username;
+
+    @Getter
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public User() { this.roles = new HashSet<>();}
+
+    public User(String username, String password) {
+        this();
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password, List<Role> roles) {
+        this(username, password);
+        addRoles(roles);
+    }
+
+    public User addRole(Role role) {
+        this.roles.add(role);
+        return this;
+    }
+
+    public User addRoles(List<Role> roles) {
+        var validatedRoles = Role.validateRoleSet(roles);
+        this.roles.addAll(validatedRoles);
+        return this;
+    }
+}
