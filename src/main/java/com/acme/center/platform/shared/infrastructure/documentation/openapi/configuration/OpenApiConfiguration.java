ea5c25c158407cf10e36e245a58067e965fb0250
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-06 08:45:58 -0500

    feat(iam): added open api security configuration.

--- src/main/java/com/acme/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,9 +1,12 @@
 package com.acme.center.platform.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +26,18 @@ public class OpenApiConfiguration {
                 .externalDocs(new ExternalDocumentation()
                         .description("ACME Learning Platform Wiki Documentation")
                         .url("https://acme-learning-platform.wiki.github.io/docs"));
+        // Add Security Scheme
+        final String securitySchemeName = "bearerAuth";
+        openApi.addSecurityItem(new SecurityRequirement()
+                .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+        // Return OpenAPI configuration object
         return openApi;
     }
 }
